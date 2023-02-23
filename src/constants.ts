7bf92c2413066d6cd28718d6e8f245effc9b1167
Kaczanowski Mateusz <kaczanowski.mateusz@gmail.com>
2023-02-23 18:58:28 +0100

    feat: configurable tx wait time

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const isDeepCheckEnabled = false
 export const gracePeriod = {enabled: false, eras: 0}
 export const runAttempts = 3
 export const claimAttempts = 3
+export const txWaitTimeMs = 60000;
